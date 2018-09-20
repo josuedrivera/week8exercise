7029b11981858f66d8ca675ddca88b1ed84a8590
JOSUE RIVERA <josuerivera@JOSUEs-Air.hitronhub.home>
2018-09-20 00:48:22 -0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
-var userAge = prompt(“how old are you?”);
+console.log("connected");
+
+
+
+
+var userAge = prompt("How old are you?");
 
 if(userAge >= 21) {
-	console.log(“Welcome, you may enter”);
+	console.log("Welcome, you may enter. Drink responsibly!");
 }
 else if(userAge == 21) {
-	console.log(“Congratulations on turning 21! You may enter”);
+	console.log("Congratulations on turning 21! You may enter and drink responsibly");
 }
 else if(userAge >= 18) {
-	console.log(“Welcome, you may enter but you can NOT drink!”);
+	console.log("Welcome, you may enter but you can NOT drink!");
 }
 else if(userAge == 18) {
-	console.log(“Congratulations on turning 18! You may enter, but you can NOT drink!”);
+	console.log("Congratulations on turning 18! You may enter, but you can NOT drink!");
 }
 else {
-	console.log(“You shall NOT pass!”);
+	console.log("You shall NOT pass!");
 }
